54c1c06e72f31680884af92ce2ac64df40aed90e
Qi <yingqi87@gmail.com>
2021-03-08 17:15:44 -0600

    second day

---
+++ src/app/article.ts
@@ -0,0 +1,10 @@
+export class Article {
+    id!: number;
+    title='';
+    tag='';
+    content='';
+    date: Date = new Date();
+    description='';
+    imageUrl='';
+
+}
---
+++ src/app/mock-articles.ts
@@ -0,0 +1,25 @@
+import { Article } from "./article";
+
+export const ARTICLES: Article[]=[
+{
+id:1,
+title:"My First Article",
+tag:"my first article",
+date: new Date(),
+content:'',
+description:"Please read my first article",
+imageUrl:'http://angular.io/assets/images/logos/angular/angular.png'
+},
+
+{
+    id:2,
+    title:"My Second Article",
+    tag:"my second article",
+    date: new Date(),
+    content:'',
+    description:"Please read my second article",
+    imageUrl:'http://angular.io/assets/images/logos/angular/angular.png'
+}
+
+];
+
